0e5dcda3737f458727e074068da23206abde3748
kaizen011 <kaizen011@users.noreply.github.com>
2024-01-19 16:27:53 +0100

    Unfinished foundation logic calculation

--- app.js
+++ app.js
@@ -1,28 +1,76 @@
 const resultDiv = document.getElementById('result');
 const buttons = document.querySelectorAll('.buttons');
 const clearBtn = document.getElementById('clear');
-const operandBtns = document.querySelectorAll('.buttons operand');
+const operandBtns = document.querySelectorAll('.buttons.operand');
 const equalBtn = document.querySelector('.buttons.equal');
 
 
 resultDiv.textContent = '';
 
-
+let firstNum;
+let secondNum;
 
 
 buttons.forEach( function(button){
+
    button.addEventListener("click", function() {
-    resultDiv.textContent += button.textContent;
+    if(!button.classList.contains('operand') && !button.classList.contains('equal'))
+    {
+        resultDiv.textContent += button.textContent;
+         firstNum = (parseFloat(resultDiv));
+         secondNum = (parseFloat(resultDiv));
+    }
    });
+
+
 });
 
 
-buttons.forEach( function(button){
-    button.addEventListener("click", function() {
-         
+console.log(firstNum);
+console.log(secondNum);
+
+operandBtns.forEach( function(operandi){
+    operandi.addEventListener("click", function() {
+         if(!operandi.classList.contains('equal')){
+            
+            if(operandi.textContent == '+'){
+                resultDiv.textContent += operandi.textContent;
+            }
+            if(operandi.textContent == '-'){
+                resultDiv.textContent += operandi.textContent;
+            }
+
+            if(operandi.textContent == '*'){
+                resultDiv.textContent += operandi.textContent;
+            }
+
+            if(operandi.textContent == '/'){
+                resultDiv.textContent += operandi.textContent;
+            }
+
+
+
+         }
     });
  });
  
+
+ equalBtn.addEventListener("click", function(){
+    if(operandi.textContent == '+'){
+        
+    }
+    if(operandi.textContent == '-'){
+        
+    }
+
+    if(operandi.textContent == '*'){
+        
+    }
+
+    if(operandi.textContent == '/'){
+        
+    }
+ });
 
 
 clearBtn.addEventListener("click", function() {
